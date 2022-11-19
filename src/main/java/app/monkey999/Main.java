4e31a4f8a337d1cdf9b03e680c6c953d7fb4add4
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-11-19 20:07:45 +0900

    add help

--- src/main/java/app/monkey999/Main.java
+++ src/main/java/app/monkey999/Main.java
@@ -15,6 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,30 +23,51 @@ import java.util.regex.Pattern;
 public class Main {
 
     /**
-     *
      * @param args args[0]: 変換対象ファイル args[1]: ファイルの言語 例: javascript
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
 
-        System.out.println("args[0]" + args[0]);
-        System.out.println("args[1]" + args[1].toLowerCase());
+        // show help
+        var top = " ";
+        var nest1 = "   ";
+        var nest2 = "     ";
+        if (args.length == 0) {
+            System.out.println();
+            System.out.println("---Create Markdown from any source file---");
+            System.out.println("Usage: ");
 
-        Creator creator = switch (args[1].toLowerCase()) {
-            case "java" -> CreatorFactory.getInstance(CreatorName.Java);
-            case "javascript" -> CreatorFactory.getInstance(CreatorName.javascript);
-            case "php" -> CreatorFactory.getInstance(CreatorName.PHP);
-            case "powershell" -> CreatorFactory.getInstance(CreatorName.Powershell);
-            case "nextjs" -> CreatorFactory.getInstance(CreatorName.nextjs);
-            case "html" -> CreatorFactory.getInstance(CreatorName.html);
-            case "css" -> CreatorFactory.getInstance(CreatorName.css);
-            case "nomal" -> CreatorFactory.getInstance(CreatorName.nomal);
-            default -> null;
+            System.out.println(top + "mdgen filename file_type");
+            System.out.println();
+            System.out.println(nest1 + "filename: ");
+            System.out.println(nest2 + "absolute path");
+            System.out.println();
+            System.out.println(nest1 + "filetype: ");
+            Arrays.stream(CreatorName.values()).forEach((item) -> System.out.println(nest2 + item));
 
-        };
 
-        creator.message();
-        creator.create(args[0]);
+        } else {
+
+
+            System.out.println("args[0]: " + args[0]);
+            System.out.println("args[1]: " + args[1].toLowerCase());
+
+            Creator creator = switch (args[1].toLowerCase()) {
+                case "java" -> CreatorFactory.getInstance(CreatorName.Java);
+                case "javascript" -> CreatorFactory.getInstance(CreatorName.javascript);
+                case "php" -> CreatorFactory.getInstance(CreatorName.PHP);
+                case "powershell" -> CreatorFactory.getInstance(CreatorName.Powershell);
+                case "nextjs" -> CreatorFactory.getInstance(CreatorName.nextjs);
+                case "html" -> CreatorFactory.getInstance(CreatorName.html);
+                case "css" -> CreatorFactory.getInstance(CreatorName.css);
+                case "nomal" -> CreatorFactory.getInstance(CreatorName.nomal);
+                default -> null;
+
+            };
+
+            creator.message();
+            creator.create(args[0]);
+        }
 
     }
 }
